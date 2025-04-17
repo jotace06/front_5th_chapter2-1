c462c009586697938039a62831479d65ea63a330
JC Chung <jc@JCs-MacBook-Pro.local>
2025-04-18 06:35:11 +0900

    배포 관련 설정

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,32 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  // React JSX 변환을 위한 플러그인 추가
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const isProduction = command === 'build';
+  return {
+    base: isProduction ? '/front_5th_chapter2-1/' : '/',
+    plugins: [react()],
 
-  // 개발 서버 설정
-  server: {
-    open: '/', // 서버 시작시 기본 페이지
-  },
+    server: {
+      open: '/', // 서버 시작시 기본 페이지
+    },
 
-  // 빌드 설정 (멀티 페이지)
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-        basic: './index.basic.html',
-        advanced: './index.advanced.html',
+    // 빌드 설정 (멀티 페이지)
+    build: {
+      rollupOptions: {
+        input: {
+          main: './index.html',
+          basic: './index.basic.html',
+          advanced: './index.advanced.html',
+        },
       },
     },
-  },
 
-  // Vitest 테스트 설정
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: 'src/setupTests.js',
-  },
+    // Vitest 테스트 설정
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: 'src/setupTests.js',
+    },
+  };
 });
